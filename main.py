3155b82e83b66ca3827d95d90306436102cba101
root <root@beaglebone.localdomain>
2016-12-08 14:19:50 +0000

    Changed a lot of thing including the addition of the ajax jquery POST request for refreshing the webcam_image

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
-# import webcam_fetch
+from flask import Flask, render_template, request
+from PIL import Image
+from subprocess import call
+import sys
 
+imgNum = 0
+def fetch_new_image():
+
+	call("/root/usb_cam/grabber.o")
+	
+	im = Image.open("/root/PepperPlant_Website/out001.ppm")
+	
+	tmp_name = '/root/PepperPlant_Website/static/webcam.jpg'
+
+	im.save(tmp_name)
+
+	call(["rm","/root/PepperPlant_Website/out000.ppm"\
+		,"/root/PepperPlant_Website/out001.ppm"] )
+	
 app = Flask (__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index ():
+	if request.method == 'POST':
+		requestType = request.form['requestType']
+		print (requestType)
+		fetch_new_image()
 	return render_template("index.html")
 
+
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0",debug=True)
+	app.run(host = "0.0.0.0",debug = True)
--- webcam_fetch.py
+++ webcam_fetch.py
@@ -3,7 +3,7 @@ from subprocess import call
 # def fetch_image ():
 
 call("/root/usb_cam/grabber.o")
-im = Image.open('/root/usb_cam/out001.ppm')
+im = Image.open("/root/PepperPlant_Website/out001.ppm")
 im.save("/root/PepperPlant_Website/static/webcam.jpg")
-# call(["rm","/root/PepperPlant_Website/out000.ppm"\
-# 	,"/root/PepperPlant_Website/out001.ppm"] )
+call(["rm","/root/PepperPlant_Website/out000.ppm"\
+	,"/root/PepperPlant_Website/out001.ppm"] )

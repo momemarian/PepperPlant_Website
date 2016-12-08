6569955864e44aa14246b292ad770a59caf00261
root <root@beaglebone.localdomain>
2016-12-08 14:58:30 +0000

    Improved the jQuery on fetching the data

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ import sys
 imgNum = 0
 def fetch_new_image():
 
+	imgNumber = 1
+
 	call("/root/usb_cam/grabber.o")
 	
 	im = Image.open("/root/PepperPlant_Website/out001.ppm")
 	
-	tmp_name = '/root/PepperPlant_Website/static/webcam.jpg'
+	tmp_name = '/root/PepperPlant_Website/static/webcam_{0:d}.jpg'.format(imgNumber)
 
 	im.save(tmp_name)
 
 	call(["rm","/root/PepperPlant_Website/out000.ppm"\
 		,"/root/PepperPlant_Website/out001.ppm"] )
+
+	return imgNumber
 	
 app = Flask (__name__)
 
@@ -23,10 +27,12 @@ app = Flask (__name__)
 def index ():
 	if request.method == 'POST':
 		requestType = request.form['requestType']
-		print (requestType)
-		fetch_new_image()
-	return render_template("index.html")
+		if requestType == "RefreshImage":
+			return str(fetch_new_image())
+
+	imgNumber = str(fetch_new_image())
+	return render_template("index.html",imgNumber=imgNumber)
 
 
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0",debug = True)
+	app.run(host = "0.0.0.0",debug = True,threaded=True)

49a7c2f38675ca5559e043c8a44abd49d5220d16
root <root@beaglebone.localdomain>
2016-12-09 19:21:31 +0000

    so mcuh stuff mainly added timestamp check and added the v4l2grab library

--- main.py
+++ main.py
@@ -5,46 +5,47 @@ import sys
 from os import listdir
 from os.path import isfile, join
 import os
-import datetime
+import datetime, time
 
 
 def find_lates_image ():
     
     imageDir = os.getcwd() + "/static/webcam_images"
     imageFiles = listdir(imageDir)
-    
-    return max(imageFiles)
+    latestImage = max(imageFiles)
+
+    tmpStr = latestImage.split(".")[0]
+    tmpStr = tmpStr.split("_")
+    latestImageTime = datetime.datetime(2000+int(tmpStr[1]), int(tmpStr[2]), int(tmpStr[3]), int(tmpStr[4]), int(tmpStr[5]),int(tmpStr[6]))
+    return latestImageTime
+
 def fetch_new_image():
 
-    currTimeStamp = datetime.datetime.now().strftime("%y_%m_%d_%H_%M_%S")
-    # latestImage = find_lates_image()
-    
-    # print (latestImage)
+    currTime = datetime.datetime.now()
+    latestImageTime = find_lates_image()
+    timeDiff = (currTime-latestImageTime).total_seconds()
 
-    imgFileName = 'webcam_{0}.jpg'.format(currTimeStamp)
-    webSiteRoot = "/root/PepperPlant_Website/"
-    
-    call("/root/usb_cam/grabber.o")
-
-    im = Image.open(webSiteRoot+"out001.ppm")    
-    im.save('{0}static/webcam_images/{1}'.format(webSiteRoot,imgFileName))
-
-    call(["rm",webSiteRoot+"out000.ppm"\
-        ,webSiteRoot+"out001.ppm"] )
-
-
+    if (timeDiff > 10.0):
+        imgFileName = 'webcam_{0}.jpg'.format(currTime.strftime("%y_%m_%d_%H_%M_%S"))
+        webSiteRoot = os.getcwd()
+        call(["{0}/v4l2grab/v4l2grab".format(webSiteRoot), "-W", "1920", "-H", "1080", "-q", "100", "-o", '{0}/static/webcam_images/{1}'.format(webSiteRoot,imgFileName)])
+        print ("Grabbing a new frame")    
+    else:
+        imgFileName = 'webcam_{0}.jpg'.format(latestImageTime.strftime("%y_%m_%d_%H_%M_%S")) 
+        print ("Too quick")
     return imgFileName
-    
-app = Flask (__name__)
 
+app = Flask (__name__)
 @app.route("/",methods=['GET','POST'])
 def index ():
     if request.method == 'POST':
         requestType = request.form['requestType']
         if requestType == "RefreshImage":
             return fetch_new_image()
+            # return '0'
 
     imgFileName = fetch_new_image()
+    # imgFileName = '0'
     return render_template("index.html",imgFileName=imgFileName)
 
 
